7d7f4a89ff8834f16ce25bc60106678152430043
edison <2499090390@qq.com>
2017-09-17 12:14:38 +0800

    first commit

---
+++ data_util/data.py
@@ -0,0 +1,25 @@
+# _*_ coding:utf-8 _*_
+
+test_data = [{'house': 1, 'single': 1, 'income': 125, 'category': 0},
+             {'house': 0, 'single': 0, 'income': 100, 'category': 0},
+             {'house': 0, 'single': 1, 'income': 70, 'category': 0},
+             {'house': 1, 'single': 0, 'income': 120, 'category': 0},
+             {'house': 0, 'single': 2, 'income': 95, 'category': 1},
+             {'house': 0, 'single': 0, 'income': 60, 'category': 0},
+             {'house': 1, 'single': 2, 'income': 220, 'category': 0},
+             {'house': 0, 'single': 1, 'income': 85, 'category': 1},
+             {'house': 0, 'single': 0, 'income': 75, 'category': 0},
+             {'house': 0, 'single': 1, 'income': 90, 'category': 1}]
+
+
+test_data_2 = [{'log': 's', 'friend' : 's', 'head': 'no', 'category': 'no'},
+               {'log': 's', 'friend' : 'l', 'head': 'yes', 'category': 'yes'},
+               {'log': 'l', 'friend': 'm', 'head': 'yes', 'category': 'yes'},
+               {'log': 'm', 'friend': 'm', 'head': 'yes', 'category': 'yes'},
+               {'log': 'l', 'friend': 'm', 'head': 'yes', 'category': 'yes'},
+               {'log': 'm', 'friend' : 'l', 'head': 'no', 'category': 'yes'},
+               {'log': 'm', 'friend': 's', 'head': 'no', 'category': 'no'},
+               {'log': 'l', 'friend': 'm', 'head': 'no', 'category': 'yes'},
+               {'log': 'm', 'friend': 's', 'head': 'no', 'category': 'yes'},
+               {'log': 's', 'friend': 's', 'head': 'yes', 'category': 'no'}]
+
---
+++ descison_tree.py
@@ -0,0 +1,71 @@
+# _*_ coding:utf-8 _*_
+import math
+import copy
+from  data_util.data import *
+test_data = test_data_2
+def get_property(data={}):
+    property_category = {}
+    for property in data.keys():
+        property_category.setdefault()
+
+def get_category(property=None):
+    if property:
+        total_property = {}
+        for item in test_data:
+            for property in test_data[0].keys():
+                total_property.setdefault(property, {})
+                total_property[property].setdefault(item[property], {})
+
+                total_property[property][item[property]].setdefault(item['category'], 0)
+                total_property[property][item[property]][item['category']] += 1
+        return total_property
+    p = {}
+    count = 0
+    for item in test_data:
+        count += 1
+        p.setdefault(item['category'], 0)
+        p[item['category']] += 1
+
+    return p
+def get_entropy(property=None):
+    p = copy.deepcopy(property)
+    if not p or not isinstance(p, dict):
+        return
+    count = 0
+    for value in p.values():
+        count += value
+    if count == 0:
+        return
+    for key, value in p.items():
+        temp = value / count
+        p[key] = -(temp * math.log(temp, 2))
+    return sum(p.values())*(count/10.0)
+
+def get_info(data={}, property=None):
+    if property:
+        data = data[property]
+        info_property = 0
+        for property in data.values():
+            resullt = get_entropy(property)
+            info_property += resullt
+        return info_property
+    info = {}
+    for key in data.keys():
+        data_property = data[key]
+        info_property = 0
+        for property in data_property.values():
+            resullt = get_entropy(property)
+            info_property += resullt
+        info[key] = info_property
+    return info
+total_probability = get_category()
+total_info = get_entropy(total_probability)
+print(total_info)
+house_count = get_category(property=True)
+print(house_count)
+
+info = get_info(house_count)
+print(info)
+
+for info_A in info.values():
+    print(total_info - info_A)
