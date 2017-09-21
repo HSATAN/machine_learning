f660e4e056ec91a365503b254321e58cc8d3525a
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-09-21 12:01:18 +0800

    fix

--- bayes.py
+++ bayes.py
@@ -15,13 +15,13 @@ class Bayes(object):
 
     @classmethod
     def get_p(cls, count=0, category={}):
-        p = {}
+        q = {}
         if count == 0:
             return
         for key, value in category.items():
-            p.setdefault(key, .0)
-            p[key] = value/float(count)
-        return p
+            q.setdefault(key, .0)
+            q[key] = value/float(count)
+        return q
 
     @classmethod
     def statistics_condition_p(cls, attribute_list=None, data={}, category=None):
@@ -43,16 +43,48 @@ class Bayes(object):
         p = copy.deepcopy(count_p)
         for category_key, category_value in p.items():
             category_count = sum(category_value['category'].values())
+            result.setdefault(category_key, {})
             for category, value in category_value.items():
+                if category != 'category':
+                    result[category_key].setdefault(category, {})
                 for key, count in value.items():
-                    if key == 'category':
-                        return
-                    print('P(%s = %s|category=%s) = %s' %(category,key,category_key,count/category_count))
+                    if category == 'category':
+                        continue
+                    print('P(%s = %s|category=%s) = %s' %(category,key,category_key,float(count)/category_count))
+                    result[category_key][category][key] = float(count)/category_count
+        return result
+
+    @classmethod
+    def classify(cls, result={}, record={}):
+
+        """
+        返回类
+
+        :param result:
+        :param record:
+        :return:
+        """
+        p = {}
+        for category, item in result.items():
+            p.setdefault(category, 0)
+            for key, value in record.items():
+                if p[category] == 0:
+                    p[category] = item[key][value]
+                else:
+                    p[category] *= item[key][value]
+
+        return p
+        pass
 bayes = Bayes()
-p, count = bayes.get_category()
-print(p)
-print(count)
-p_total = bayes.get_p(count, p)
+pro, count = bayes.get_category()
+print(pro)
+p_total = bayes.get_p(count, pro)
 print(p_total)
 data = bayes.statistics_condition_p(data=test_data)
-bayes.calculate(data)
+result = bayes.calculate(data)
+X = {'age': 'y', 'income': 'm', 'student': 'y', 'credit_rating': 'f'}
+final_p = bayes.classify(result, X)
+
+final = {}
+for key, value in final_p.items():
+    final[key] = value*p_total[key]
